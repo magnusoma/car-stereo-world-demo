9a20f4f6f244a2b4aa8eea716b3a1012615bea45
Magnus Oma <magnus.oma@gmail.com>
2021-07-02 13:46:13 +0200

    Navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar-main/>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavbarMain from './components/NavbarMain.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    setup() {
+        
+    },
+    components: {
+        NavbarMain
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavbarMain.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav>
+        <ul>
+            <li><router-link to="/component-series">Komponent Serier</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavbarMain",
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+    nav {
+        border-bottom: 2px solid black;
+    }
+
+    li {
+    list-style-type: none;
+}
+</style>
